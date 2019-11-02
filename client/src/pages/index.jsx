8525aaa10d4c897e3de3bd14600fbd0534672f7e
Nathan Yocum <nayocum@nevada.unr.edu>
2019-11-02 09:46:33 -0700

    Initialize repo with ESLint, Prettier, and Jest

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,51 +1,34 @@
-import React from "react";
-import Helmet from "react-helmet";
-import { graphql } from "gatsby";
-import Layout from "../layout";
-import PostListing from "../components/PostListing/PostListing";
-import SEO from "../components/SEO/SEO";
-import config from "../../data/SiteConfig";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 
-class Index extends React.Component {
-  render() {
-    const postEdges = this.props.data.allMarkdownRemark.edges;
-    return (
-      <Layout>
-        <div className="index-container">
-          <Helmet title={config.siteTitle} />
-          <SEO />
-          <PostListing postEdges={postEdges} />
-        </div>
-      </Layout>
-    );
-  }
-}
+import Header from '../components/header/header';
 
-export default Index;
+const App = ({ data }) => {
+  return (
+    <div>
+      <Header title={data.site.siteMetadata.title} />
+    </div>
+  );
+};
 
-/* eslint no-undef: "off" */
-export const pageQuery = graphql`
-  query IndexQuery {
-    allMarkdownRemark(
-      limit: 2000
-      sort: { fields: [fields___date], order: DESC }
-    ) {
-      edges {
-        node {
-          fields {
-            slug
-            date
-          }
-          excerpt
-          timeToRead
-          frontmatter {
-            title
-            tags
-            cover
-            date
-          }
-        }
+App.propTypes = {
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.any
+};
+
+App.defaultProps = {
+  data: null
+};
+
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
       }
     }
   }
 `;
+
+export default App;
