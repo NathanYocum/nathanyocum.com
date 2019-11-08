22ce62405f317489f84dfc3d0dca7048e2d22b33
Nathan Yocum <nayocum@nevada.unr.edu>
2019-11-07 20:57:21 -0800

    Add Typography and Styled Components

--- client/src/components/header/header.jsx
+++ client/src/components/header/header.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+  text-align: center;
+  background-color: #0476ff;
+  color: #ffefef;
+`;
 
 const Header = ({ title }) => (
-  <div>
+  <HeaderContainer>
     <h1>{title}</h1>
-  </div>
+  </HeaderContainer>
 );
 
 Header.propTypes = {
