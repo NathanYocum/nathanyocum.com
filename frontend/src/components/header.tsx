999c0ddc540c87fb91ef268418f6fc9e05baaa9c
Nathan Yocum <nayocum@nevada.unr.edu>
2021-01-11 07:41:21 -0800

    Remove frontend

--- frontend/src/components/header.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const CustomHeader = () => (<div>Hello World!</div>)
--- frontend/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { CustomHeader } from "../components/header"
-
-export default function Home() {
-  return <CustomHeader />
-}
